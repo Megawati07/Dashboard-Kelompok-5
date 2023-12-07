d61fa23ddf49ecd30bfd6428747e2dbafb0fdf4e
egamegaa <111563368+Megawati07@users.noreply.github.com>
2023-12-07 21:41:25 +0700

    Update server.R

--- server.R
+++ server.R
@@ -11,33 +11,49 @@ server <- function(input, output)
            "Frekuensi Makan"=Makan,
            "Lemak"=Kesehatan)
   })
+  
+  # Color for contingecy table
+  col_n<-"#5703ff" #Biru
+  col_exp<-"#ff3200" #Merah
+  col_row<-"#96ceb4" #Hijau
+  col_col<-"#ffcc5c" #Kuning
+  
   output$contingency_table <- rhandsontable::renderRHandsontable({
     dataset <- datasetInput()
     dataset<-as.data.frame(table(dataset))
     dataset <- dataset %>% 
       pivot_wider(names_from = colnames(dataset)[2], values_from = colnames(dataset)[3]) %>%
       as.data.frame()
+    rownames(dataset)<-dataset[,1]
+    dataset<-dataset[,-1]
     rhandsontable(
-      dataset,height =  100,width = 300,minRows=2,minCols=2,
+      dataset,height =  100,width = 370,minRows=2,minCols=2,
       readOnly = F,stretchH = "all")
   })
+  
   output$html <- renderUI({
     dataset <- datasetInput()
     label <- colnames(dataset)
-    HTML(tab_xtab(var.row = dataset[,1], var.col = dataset[,2],tdcol.n = "red",
+    HTML(tab_xtab(var.row = dataset[,1], var.col = dataset[,2],
                   var.labels = c(label[1], label[2]),
                   title = "Tabel Kontingensi", show.summary=F,
-                  show.col.prc = TRUE,show.exp = TRUE)$knitr)
+                  show.row.prc = TRUE,show.exp = TRUE,show.col.prc = TRUE,
+                  tdcol.n = col_n,
+                  tdcol.expected = col_exp,
+                  tdcol.row = col_row,
+                  tdcol.col = col_col,
+    )$knitr)
   })
+  
   output$plotbar = renderPlotly({
     dataset <- datasetInput()
     label <- colnames(dataset)
     dataset2 <- dataset %>% group_by(assign(label[1],dataset[,1]),assign(label[2],dataset[,2])) %>% 
       summarise(Frekuensi=n()) %>% as.data.frame()
-    kat<-levels(dataset2[,2])
+    kat<-levels(dataset2[,1])
     dataset2 <- data.frame(V1=factor(unique(dataset[,2]),levels=levels(dataset[,2])),
-                           V2=dataset2[dataset2$`assign(label[2], dataset[, 2])`==kat[1],"Frekuensi"],
-                           V3=dataset2[dataset2$`assign(label[2], dataset[, 2])`==kat[2],"Frekuensi"])
+                           V2=dataset2[dataset2$`assign(label[1], dataset[, 1])`==kat[1],"Frekuensi"],
+                           V3=dataset2[dataset2$`assign(label[1], dataset[, 1])`==kat[2],"Frekuensi"])
     label1 <- levels(dataset[,1])
     colnames(dataset2)<-c(label[2],label1[1],label1[2])
     label2 <- colnames(dataset2)
@@ -47,33 +63,35 @@ server <- function(input, output)
                           xaxis=list(title=list(text=paste0('<b> ',label[2],' </b>'))),
                           legend=list(title=list(text=paste0('<b> ',label[1],' </b>'))), barmode = 'group')
   })
+  
   output$chisq = rhandsontable::renderRHandsontable({
     dataset <- datasetInput()
     hasil_chisq<-chisq.test(dataset[,2], dataset[,1], correct=FALSE)
-    data_hasil_chisq<-data.frame(Ukuran=c("Khi Kuadrat Hitung","Derajat Bebas","p-value"),
+    data_hasil_chisq<-data.frame(Ukuran=c("Khi Kuadrat Hitung","Khi Kuadrat Tabel","Derajat Bebas","p-value"),
                                  Hasil=c(round(hasil_chisq$statistic,3),
+                                         round(qchisq(0.95,df=hasil_chisq$parameter),3),
                                          round(hasil_chisq$parameter,3),
                                          round(hasil_chisq$p.value,3)))
     rownames(data_hasil_chisq)<-NULL
     rhandsontable(
-      data_hasil_chisq,height =  100,width = 300,
+      data_hasil_chisq,height =  200,width = 400,
       readOnly = T,stretchH = "all") %>% hot_col("Hasil", format = "0.000")
   })
   
-  output$risk = rhandsontable::renderRHandsontable({
+  risk = rhandsontable::renderRHandsontable({
     dataset <- datasetInput()
     hasil_riskr<-riskratio.wald(dataset[,2],dataset[,1])
-    data_hasil_riskr<-data.frame(Ukuran=c("Rasio Kejadian","Batas Bawah (SK)","Batas Atas (SK)"),
+    data_hasil_riskr<-data.frame(Ukuran=c("Resiko Relatif","Batas Bawah (SK)","Batas Atas (SK)"),
                                  Hasil=c(round(hasil_riskr$measure[2,1],3),
                                          round(hasil_riskr$measure[2,2],3),
                                          round(hasil_riskr$measure[2,3],3)))
     rownames(data_hasil_riskr)<-NULL
     rhandsontable(
-      data_hasil_riskr,height =  100,width = 300,
+      data_hasil_riskr,height =  100,width = 400,
       readOnly = T,stretchH = "all") %>% hot_col("Hasil", format = "0.000")
   })
   
-  output$odd = rhandsontable::renderRHandsontable({
+  odd = rhandsontable::renderRHandsontable({
     dataset <- datasetInput()
     hasil_odd<-oddsratio.wald(dataset[,2],dataset[,1])
     data_hasil_odd<-data.frame(Ukuran=c("Rasio Odd","Batas Bawah (SK)","Batas Atas (SK)"),
@@ -82,10 +100,9 @@ server <- function(input, output)
                                        round(hasil_odd$measure[2,3],3)))
     rownames(data_hasil_odd)<-NULL
     rhandsontable(
-      data_hasil_odd,height =  100,width = 300,
+      data_hasil_odd,height =  100,width = 400,
       readOnly = T,stretchH = "all") %>% hot_col("Hasil", format = "0.000")
   })
+  
 }
-
-
 shinyApp(ui, server)
